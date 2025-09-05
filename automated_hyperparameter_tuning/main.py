34c2dc0970089b5b09818887b34cbcf6114a587c
Adxell <adxell02@gmail.com>
2025-09-04 21:12:23 -0500

    feat: Added automated_hyperparameter_tuning

--- automated_hyperparameter_tuning/main.py
+++ automated_hyperparameter_tuning/main.py
@@ -50,3 +50,44 @@ accuracy_grid = accuracy_score(y_test, y_pred_grid)
 
 print(f"Test accuracy (GridSearchCV): {accuracy_grid:.4f}")
 print("\n Classificacion report: \n", classification_report(y_test, y_pred_grid))
+
+
+# Define paramter distribution
+param_dist = {
+    'C': np.logspace(-3, 3, 10), 
+    'kernel': ['linear', 'rbf', 'poly', 'sigmoid'], 
+    'gamma': ['scale', 'auto']
+}
+
+# Initialize RandomizedSearchCV
+random_search = RandomizedSearchCV(
+    estimator=SVC(random_state=42), 
+    param_distributions=param_dist, 
+    n_iter=20,
+    scoring='accuracy',
+    cv=5,
+    n_jobs=-1,
+    random_state=42,
+)
+
+# Performen randomized Search
+
+random_search.fit(X_train, y_train)
+
+# Best paramters and score
+best_params_random = random_search.best_params_
+best_score_radom = random_search.best_score_
+
+print(f"Best Paramters (RandomizedSearchCV): {best_params_random}")
+print(f"Best Cross-Validacion Accuracy (RandomizedSearchCV): {best_score_radom:.4f}")
+
+# Get best model
+best_random_model = random_search.best_estimator_
+
+# Predict and evaluate 
+y_pred_random = best_random_model.predict(X_test)
+
+accuracy_random = accuracy_score(y_test, y_pred_random)
+
+print(f"Test Accuracy (RandomizedSearchCV): {accuracy_random:.4f}")
+print("\n Classification Report: \n", classification_report(y_test, y_pred_random))
